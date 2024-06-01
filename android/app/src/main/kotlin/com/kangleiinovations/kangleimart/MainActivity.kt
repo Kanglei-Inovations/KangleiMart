86f782f9799b32a38b28aa52d2da7be2fcbc8c47
PRINTONEX <printonex.in@gmail.com>
2024-06-01 14:18:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/kangleiinovations/kangleimart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kangleiinovations.kangleimart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bottom_nabvar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ButtomNavbar extends StatefulWidget {
+  const ButtomNavbar({super.key});
+
+  @override
+  State<ButtomNavbar> createState() => _ButtomNavbarState();
+}
+
+class _ButtomNavbarState extends State<ButtomNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
